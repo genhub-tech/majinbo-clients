b466cc4f8cec74ebf939484ea2a04a71ef99d0c7
Genhub Tech <165643316+genhub-tech@users.noreply.github.com>
2024-06-21 00:42:25 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -13,8 +13,9 @@ OS=$(uname -s)
 if [ "$ARCH" = "x86_64" ] && [ "$OS" = "Linux" ]; then
     RELEASE_URL="$RELEASE_URL_BASE/majinbo-linux-amd64"
 elif [ "$ARCH" = "aarch64" ] && [ "$OS" = "Linux" ]; then
-    RELEASE_URL="$RELEASE_URL_BASE/majinbo-linux-aarch64"
-elif [ "$ARCH" = "aarch64" ] && [ "$OS" = "Darwin" ]; then
+    echo "The platform linux-aarch64 is not available yet."
+    exit 1
+elif [ "$ARCH" = "arm64" ] && [ "$OS" = "Darwin" ]; then
     RELEASE_URL="$RELEASE_URL_BASE/majinbo-darwin-aarch64"
 else
     echo "Unsupported platform: $OS $ARCH. The platform is not available yet."
