6b564b88ac3b046eb5ed329622dfcc1d26188b57
Genhub Tech <165643316+genhub-tech@users.noreply.github.com>
2024-06-21 00:37:12 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 
-RELEASE_URL_BASE="https://github.com/genhub-tech/majinbo-clients/releases/download"
+RELEASE_URL_BASE="https://raw.githubusercontent.com/genhub-tech/majinbo-clients/master/clients"
 VERSION="latest"
 YES=""
 
-trap 'rm -f "/tmp/exec.$$"' 0
+trap 'rm -f /tmp/exec.$$' 0
 trap 'exit $?' 1 2 3 15
 
 ARCH=$(uname -m)
 OS=$(uname -s)
 
 if [ "$ARCH" = "x86_64" ] && [ "$OS" = "Linux" ]; then
-    RELEASE_URL="$RELEASE_URL_BASE/$VERSION/majinbo-linux-amd64"
+    RELEASE_URL="$RELEASE_URL_BASE/majinbo-linux-amd64"
 elif [ "$ARCH" = "aarch64" ] && [ "$OS" = "Linux" ]; then
-    RELEASE_URL="$RELEASE_URL_BASE/$VERSION/majinbo-linux-aarch64"
+    RELEASE_URL="$RELEASE_URL_BASE/majinbo-linux-aarch64"
 elif [ "$ARCH" = "aarch64" ] && [ "$OS" = "Darwin" ]; then
-    RELEASE_URL="$RELEASE_URL_BASE/$VERSION/majinbo-darwin-aarch64"
+    RELEASE_URL="$RELEASE_URL_BASE/majinbo-darwin-aarch64"
 else
-    echo "Unsupported platform: $OS $ARCH"
+    echo "Unsupported platform: $OS $ARCH. The platform is not available yet."
     exit 1
 fi
 
